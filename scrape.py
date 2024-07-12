31e3b17ba0f912d73103e5c80085772a6211bf0a
vox <108256089+v-vox@users.noreply.github.com>
2024-07-12 11:22:13 -0400

    Update scrape.py
    changed so that it returns the values for the google sheets program

--- scrape.py
+++ scrape.py
@@ -3,19 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
 from generateTweets import generate_tweets
+from bs4 import BeautifulSoup
 import argparse 
 import time
 
 
 def get_tweets(driver, search_url, tweet_count):
     driver.get(search_url)
-    time.sleep(5)  
+    time.sleep(4)  
     body = driver.find_element(By.TAG_NAME, 'body')
-    for _ in range(8):
+    for _ in range(int(tweet_count)):
         body.send_keys(Keys.PAGE_DOWN)
-        time.sleep(2)
+        time.sleep(1)
 
     # parse with bs
     soup = BeautifulSoup(driver.page_source, 'html.parser')
@@ -28,24 +28,26 @@ def get_tweets(driver, search_url, tweet_count):
 
     return tweets
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("search_urls", type=str, help="Comma-separated list of search URLs")
-    args = parser.parse_args()
-    search_urls = args.search_urls.split(',')
+def main(links, tweets):
 
-    tweet_count = 5 #number of tweets per topic
+    search_urls = links.split(',')
+    tweet_count = tweets #number of tweets per topic
 
     # initialize selenium
     chrome_options = Options()
-    # chrome_options.add_argument("--headless")
+    chrome_options.add_argument(r"--user-data-dir=REPLACE_WITH_PATH") #e.g. chrome data directory 
+    chrome_options.add_argument(r'--profile-directory=Profile 1') #e.g. insert profile name
+    # chrome_options.add_argument("--headless") 
 
     webdriver_service = ChromeService()
     driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
 
     # log in (automate?)
-    driver.get('https://x.com/login')
-    input(" \n \n log in to Twitter, press Enter to continue... \n \n ")
+    # driver.get('https://x.com/login')
+    # input(" \n \n log in to Twitter, press Enter to continue... \n \n ")
+
+    # get tweets for each search URL
+    all_tweets = []
 
     # get tweets for each search URL
     for search_url in search_urls:
@@ -57,10 +59,10 @@ if __name__ == "__main__":
     # Combine all tweets into a single string
     all_tweets_string = "\n".join(all_tweets)
 
-    print(all_tweets_string)
-
     # Run generate_tweets on the combined tweets string
     generated_tweet = generate_tweets(all_tweets_string)
 
     # Print the returned string value
     print(generated_tweet)
+
+    return generated_tweet
